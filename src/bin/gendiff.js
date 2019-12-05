93efdee999782bb1c6bd7536870dbfd06c36eae4
andryushque <andrew.ustinov23@gmail.com>
2019-12-05 20:16:35 +0300

    update gendiff.js, add '--format' option, step#2

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,7 +2,12 @@
 import program from 'commander';
 
 program
-  .version('0.0.1')
+  .version('0.0.2')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log(gendiff(firstConfig, secondConfig, program.format));
+  });
 
 program.parse(process.argv);
