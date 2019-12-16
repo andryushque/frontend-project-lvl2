765a329aa94d4c96f3491e77288c94a17fc73e09
andryushque <andrew.ustinov23@gmail.com>
2019-12-16 16:20:40 +0300

    fix names, step #7

--- src/index.js
+++ src/index.js
@@ -1,30 +1,13 @@
-import _ from 'lodash';
 import parseFile from './parsers';
+import buildAST from './buildAST';
+import render from './render';
 
 const gendiff = (filePathBefore, filePathAfter) => {
-  const file1 = parseFile(filePathBefore);
-  const file2 = parseFile(filePathAfter);
-  const keys1 = Object.keys(file1);
-  const keys2 = Object.keys(file2);
-
-  const fileKeys = _.union(keys1, keys2);
-
-  const reduceFunc = (acc, key) => {
-    if (_.has(file1, key) === true && _.has(file2, key) === true) {
-      if (file1[key] === file2[key]) {
-        return `${acc}\r\n    ${key}: ${file1[key]}`;
-      }
-      return `${acc}\r\n  + ${key}: ${file2[key]}\r\n  - ${key}: ${file1[key]}`;
-    }
-
-    if (_.has(file1, key) === false) {
-      return `${acc}\r\n  + ${key}: ${file2[key]}`;
-    }
-    return `${acc}\r\n  - ${key}: ${file1[key]}`;
-  };
-
-  const result = fileKeys.reduce(reduceFunc, '');
-  return `{${result}\r\n}\r\n`;
+  const fileBefore = parseFile(filePathBefore);
+  const fileAfter = parseFile(filePathAfter);
+  const ast = buildAST(fileBefore, fileAfter);
+  const result = render(ast);
+  return result;
 };
 
 export default gendiff;
