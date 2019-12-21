8f8917f2b3a3d975ca3782fa2b550155044bc77d
andryushque <andrew.ustinov23@gmail.com>
2019-12-21 20:05:51 +0300

    update tree renderer

--- src/formatters/renderTree.js
+++ src/formatters/renderTree.js
@@ -23,8 +23,8 @@ const render = (ast) => {
     const getLineByType = {
       unchanged: (obj) => `${makeTab(lvl + 1)}${obj.key}: ${stringify(obj.value, lvl + 1)}\n`,
       added: (obj) => `${makeTab(lvl)}${plus}${obj.key}: ${stringify(obj.value, lvl + 1)}\n`,
-      removed: (obj) => `${makeTab(lvl)}${minus}${obj.key}: ${stringify(obj.value, lvl + 1)}\n`,
       changed: (obj) => `${makeTab(lvl)}${plus}${obj.key}: ${stringify(obj.valueAfter, lvl + 1)}\n${makeTab(lvl)}${minus}${obj.key}: ${stringify(obj.valueBefore, lvl + 1)}\n`,
+      removed: (obj) => `${makeTab(lvl)}${minus}${obj.key}: ${stringify(obj.value, lvl + 1)}\n`,
       object: (obj) => `${makeTab(lvl + 1)}${obj.key}: {\n${iterAst(obj.children, lvl + 1)}${makeTab(lvl + 1)}}\n`,
     };
     return item.reduce((acc, obj) => acc.concat(getLineByType[obj.type](obj)), '');
