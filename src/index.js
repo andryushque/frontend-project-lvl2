7ae5c304b8f01853cb4ddff29985528d6bcf8412
andryushque <andrew.ustinov23@gmail.com>
2019-12-21 15:43:51 +0300

    add json renderer

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import parseFile from './parsers';
 import buildAST from './buildAST';
 import renderTree from './formatters/renderTree';
 import renderPlain from './formatters/renderPlain';
+import renderJson from './formatters/renderJson';
 
 const renderFormats = {
   tree: renderTree,
   plain: renderPlain,
+  json: renderJson,
 };
 
 const getRenderFormat = (format) => renderFormats[format];
