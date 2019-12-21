e1426e81605686fc179d42708991702eeed168ab
andryushque <andrew.ustinov23@gmail.com>
2019-12-21 15:04:44 +0300

    add plain render

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,12 +3,12 @@ import program from 'commander';
 import gendiff from '..';
 
 program
-  .version('0.0.3')
+  .version('1.1.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
   .action((firstConfig, secondConfig) => {
-    console.log(gendiff(firstConfig, secondConfig));
+    console.log(gendiff(firstConfig, secondConfig, program.format));
   });
 
 program.parse(process.argv);
