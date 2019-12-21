b95d08a776ea2433153dbf3fb0b43c51f0ecf9fc
andryushque <andrew.ustinov23@gmail.com>
2019-12-21 15:42:46 +0300

    update ast builder

--- src/buildAST.js
+++ src/buildAST.js
@@ -1,48 +1,26 @@
 import _ from 'lodash';
 
 const buildAST = (fileBefore, fileAfter) => {
-  const keys1 = _.keys(fileBefore);
-  const keys2 = _.keys(fileAfter);
-  const fileKeys = _.union(keys1, keys2);
-
+  const fileKeys = _.union(_.keys(fileBefore), _.keys(fileAfter));
   const iterAST = (fileKey) => {
     if (_.isEqual(fileBefore[fileKey], fileAfter[fileKey])) {
-      return {
-        key: fileKey,
-        value: fileBefore[fileKey],
-        type: 'unchanged',
-      };
+      return { key: fileKey, value: fileBefore[fileKey], type: 'unchanged' };
     }
 
     if (!_.has(fileBefore, fileKey) && _.has(fileAfter, fileKey)) {
-      return {
-        key: fileKey,
-        value: fileAfter[fileKey],
-        type: 'added',
-      };
+      return { key: fileKey, value: fileAfter[fileKey], type: 'added' };
     }
 
     if (_.has(fileBefore, fileKey) && !_.has(fileAfter, fileKey)) {
-      return {
-        key: fileKey,
-        value: fileBefore[fileKey],
-        type: 'removed',
-      };
+      return { key: fileKey, value: fileBefore[fileKey], type: 'removed' };
     }
 
     if (_.isObject(fileBefore[fileKey]) && _.isObject(fileAfter[fileKey])) {
-      return {
-        key: fileKey,
-        type: 'object',
-        children: buildAST(fileBefore[fileKey], fileAfter[fileKey]),
-      };
+      return { key: fileKey, type: 'object', children: buildAST(fileBefore[fileKey], fileAfter[fileKey]) };
     }
 
     return {
-      key: fileKey,
-      valueBefore: fileBefore[fileKey],
-      valueAfter: fileAfter[fileKey],
-      type: 'changed',
+      key: fileKey, valueBefore: fileBefore[fileKey], valueAfter: fileAfter[fileKey], type: 'changed',
     };
   };
   const result = fileKeys.map(iterAST);
