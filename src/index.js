bf014a1c00961ee7fd479f578b611fcb16de13d4
andryushque <andrew.ustinov23@gmail.com>
2019-12-21 23:02:35 +0300

    update index.js, add different types error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import path from 'path';
 import parseFile from './parsers';
 import buildAST from './buildAST';
 import getRenderFormat from './formatters';
 
 const gendiff = (filePathBefore, filePathAfter, format = 'tree') => {
+  if (path.extname(filePathBefore) !== path.extname(filePathAfter)) {
+    throw new Error('Please, use files with the same format!');
+  }
   const fileBefore = parseFile(filePathBefore);
   const fileAfter = parseFile(filePathAfter);
   const ast = buildAST(fileBefore, fileAfter);
