f149b75c5ec93a768d085646c1376e4525c3e94c
andryushque <andrew.ustinov23@gmail.com>
2019-12-24 17:55:38 +0300

    refactoring tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,24 +2,26 @@ import fs from 'fs';
 import path from 'path';
 import gendiff from '../src';
 
-const dataFormats = ['json', 'yml', 'ini'];
+const fileExtensions = ['json', 'yml', 'ini'];
 const testDirectory = path.join(__dirname, '/__fixtures__/');
-const getData = (fileName) => fs.readFileSync(`${testDirectory}${fileName}`, 'utf-8').replace(/\r/g, '');
+const getPathToFileBefore = (fileExtension) => path.join(testDirectory, `before.${fileExtension}`);
+const getPathToFileAfter = (fileExtension) => path.join(testDirectory, `after.${fileExtension}`);
+const getData = (fileName) => fs.readFileSync(path.join(testDirectory, fileName), 'utf-8').replace(/\r/g, '');
 
-test.each(dataFormats)('gendiff test - tree format (default)', (dataFormat) => {
-  const received = gendiff(`${testDirectory}/before.${dataFormat}`, `${testDirectory}/after.${dataFormat}`);
+test.each(fileExtensions)('gendiff test - tree format (default)', (fileExtension) => {
+  const received = gendiff(getPathToFileBefore(fileExtension), getPathToFileAfter(fileExtension));
   const expected = getData('treeDifference.txt');
   expect(received).toEqual(expected);
 });
 
-test.each(dataFormats)('gendiff test - plain format', (dataFormat) => {
-  const received = gendiff(`${testDirectory}/before.${dataFormat}`, `${testDirectory}/after.${dataFormat}`, 'plain');
+test.each(fileExtensions)('gendiff test - plain format', (fileExtension) => {
+  const received = gendiff(getPathToFileBefore(fileExtension), getPathToFileAfter(fileExtension), 'plain');
   const expected = getData('plainDifference.txt');
   expect(received).toEqual(expected);
 });
 
-test.each(dataFormats)('gendiff test - json format', (dataFormat) => {
-  const received = gendiff(`${testDirectory}/before.${dataFormat}`, `${testDirectory}/after.${dataFormat}`, 'json');
+test.each(fileExtensions)('gendiff test - json format', (fileExtension) => {
+  const received = gendiff(getPathToFileBefore(fileExtension), getPathToFileAfter(fileExtension), 'json');
   const expected = getData('jsonDifference.txt').trim();
   expect(received).toEqual(expected);
 });
