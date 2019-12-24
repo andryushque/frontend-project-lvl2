6d014daadcfbf3e040d9e4008a489a75625ee047
andryushque <andrew.ustinov23@gmail.com>
2019-12-24 17:07:55 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import path from 'path';
 import parseFile from './parsers';
 import buildAST from './buildAST';
 import getRenderFormat from './formatters';
 
 const gendiff = (filePathBefore, filePathAfter, format = 'tree') => {
-  if (path.extname(filePathBefore) !== path.extname(filePathAfter)) {
-    throw new Error('Please, use files with the same extension!');
-  }
   const fileBefore = parseFile(filePathBefore);
   const fileAfter = parseFile(filePathAfter);
   const ast = buildAST(fileBefore, fileAfter);
