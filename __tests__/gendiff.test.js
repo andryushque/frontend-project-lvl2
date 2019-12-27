07bb9fc08ba04974c7404bc690a8df99ee6418a5
andryushque <andrew.ustinov23@gmail.com>
2019-12-27 11:43:55 +0300

    refactoring tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,8 +4,8 @@ import gendiff from '../src';
 
 const fileExtensions = ['.json', '.yml', '.ini'];
 const testDirectory = path.join(__dirname, '/__fixtures__/');
-const getPathToFile = (fileName, fileExt = '') => path.join(testDirectory, fileName + fileExt);
-const getData = (fileName) => fs.readFileSync(getPathToFile(fileName), 'utf-8').replace(/\r/g, '');
+const getPathToFile = (fileName, fileExt = '') => path.join(testDirectory, `${fileName}${fileExt}`);
+const getData = (fileName) => fs.readFileSync(getPathToFile(fileName), 'utf-8');
 
 test.each(fileExtensions)('gendiff test - tree format (default)', (fileExt) => {
   const received = gendiff(getPathToFile('before', fileExt), getPathToFile('after', fileExt));
@@ -21,6 +21,6 @@ test.each(fileExtensions)('gendiff test - plain format', (fileExt) => {
 
 test.each(fileExtensions)('gendiff test - json format', (fileExt) => {
   const received = gendiff(getPathToFile('before', fileExt), getPathToFile('after', fileExt), 'json');
-  const expected = getData('jsonDifference.txt').trim();
+  const expected = getData('jsonDifference.txt');
   expect(received).toEqual(expected);
 });
