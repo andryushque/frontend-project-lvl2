3215f953db58ace1e71c279b5ee4ed94a0ee5fbf
andryushque <andrew.ustinov23@gmail.com>
2020-01-08 15:16:53 +0300

    refactoring renderPlain

--- src/formatters/renderPlain.js
+++ src/formatters/renderPlain.js
@@ -13,11 +13,11 @@ const stringify = (value) => {
 const render = (ast) => {
   const iterAst = (item, parentKey) => {
     const getKey = (obj) => [...parentKey, obj.key];
-    const getProperty = (obj) => `'${getKey(obj).join('.')}'`;
+    const getProperty = (obj) => getKey(obj).join('.');
     const getLineByType = {
-      added: (obj) => `Property ${getProperty(obj)} was added with value: ${stringify(obj.value)}`,
-      removed: (obj) => `Property ${getProperty(obj)} was removed`,
-      changed: (obj) => `Property ${getProperty(obj)} was updated. From ${stringify(obj.valueBefore)} to ${stringify(obj.valueAfter)}`,
+      added: (obj) => `Property '${getProperty(obj)}' was added with value: ${stringify(obj.value)}`,
+      removed: (obj) => `Property '${getProperty(obj)}' was removed`,
+      changed: (obj) => `Property '${getProperty(obj)}' was updated. From ${stringify(obj.valueBefore)} to ${stringify(obj.valueAfter)}`,
       object: (obj) => iterAst(obj.children, getKey(obj)),
     };
     const line = item.filter((obj) => obj.type !== 'unchanged')
